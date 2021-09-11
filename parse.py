d387249406d4b6365ba86923f696ad11604cbbac
Shilong Dai <sdai@viperfish.net>
2021-09-10 20:02:43 -0400

    adds more command

--- parse.py
+++ parse.py
@@ -10,6 +10,15 @@ NEWLINE = 4
 EOF = -1
 UNREC = -2
 
+# Error Types
+COMMAND_UNREC = "500 Syntax error: command unrecognized"
+PARAM_ERROR = "501 Syntax error in parameters or arguments"
+OUT_OF_ORDER_ERROR = "503 Bad sequence of commands"
+
+# OK Types
+OK = "250 OK"
+ENTER_DATA = "354 Start mail input; end with <CRLF>.<CRLF>"
+
 Token = namedtuple("Token", ["type", "spelling"])
 
 
@@ -51,8 +60,34 @@ class TokenScanner:
 
 class ParseException(BaseException):
 
-    def __init__(self, symbol):
-        BaseException.__init__(self, "ERROR -- %s" % symbol)
+    def __init__(self, error):
+        BaseException.__init__(self, error)
+
+
+class UnrecognizedCommandException(ParseException):
+
+    def __init__(self):
+        ParseException.__init__(self, COMMAND_UNREC)
+
+
+class ParameterErrorException(ParseException):
+
+    def __init__(self):
+        ParseException.__init__(self, PARAM_ERROR)
+
+
+class OutOfOrderException(ParseException):
+
+    def __init__(self):
+        ParseException.__init__(self, OUT_OF_ORDER_ERROR)
+
+
+class Mail:
+
+    def __init__(self, src, rcpts, text):
+        self.src = src
+        self.targets = rcpts
+        self.text = text
 
 
 def accept_literal_str(scanner, string):
@@ -63,24 +98,26 @@ def accept_literal_str(scanner, string):
 
 
 def parse_mail_from_cmd(scanner):
-    if not accept_literal_str(scanner, "MAIL"):
-        raise ParseException("mail-from-cmd")
-
-    parse_whitespace(scanner)
-
-    if not accept_literal_str(scanner, "FROM:"):
-        raise ParseException("mail-from-cmd")
+    try:
+        if not accept_literal_str(scanner, "MAIL"):
+            raise UnrecognizedCommandException()
+        parse_whitespace(scanner)
+        if not accept_literal_str(scanner, "FROM:"):
+            raise UnrecognizedCommandException()
+    except ParseException:
+        raise UnrecognizedCommandException()
 
     parse_nullspace(scanner)
-    parse_path(scanner)
+    path = parse_path(scanner)
     parse_nullspace(scanner)
     if not scanner.accept(NEWLINE):
-        raise ParseException("mail-from-cmd")
+        raise ParameterErrorException()
+    return path
 
 
 def parse_whitespace(scanner):
     if not scanner.accept(SPACE):
-        raise ParseException("whitespace")
+        raise ParameterErrorException()
     while scanner.current.type == SPACE:
         scanner.accept(SPACE)
 
@@ -92,75 +129,200 @@ def parse_nullspace(scanner):
 
 def parse_path(scanner):
     if not scanner.accept(SPEC, "<"):
-        raise ParseException("path")
-    parse_mailbox(scanner)
+        raise ParameterErrorException()
+    mail_box = parse_mailbox(scanner)
     if not scanner.accept(SPEC, ">"):
-        raise ParseException("path")
+        raise ParameterErrorException()
+    return mail_box
 
 
 def parse_mailbox(scanner):
-    parse_string(scanner)
+    user = parse_string(scanner)
     if not scanner.accept(SPEC, "@"):
-        raise ParseException("mailbox")
-    parse_domain(scanner)
+        raise ParameterErrorException()
+    domain = parse_domain(scanner)
+    return user + "@" + domain
 
 
 def parse_string(scanner):
+    result = [scanner.current.spelling]
     if not scanner.accept(CHAR):
-        raise ParseException("string")
+        raise ParameterErrorException()
     while scanner.current.type == CHAR:
+        result.append(scanner.current.spelling)
         scanner.accept(CHAR)
+    return "".join(result)
 
 
 def parse_domain(scanner):
-    parse_element(scanner)
+    result = [parse_element(scanner)]
     if scanner.current.type == SPEC and scanner.current.spelling == ".":
+        result.append(scanner.current.spelling)
         scanner.accept(SPEC, ".")
-        parse_domain(scanner)
+        result.append(parse_domain(scanner))
+    return "".join(result)
 
 
 def accept_letter(scanner):
+    result = None
     if scanner.current.type == CHAR and scanner.current.spelling.isalpha():
+        result = scanner.current.spelling
         scanner.accept(CHAR, scanner.current.spelling)
-        return True
-    return False
+    return result
 
 
 def parse_element(scanner):
-    if not accept_letter(scanner):
-        raise ParseException("element")
+    result = []
+    letter = accept_letter(scanner)
+    if not letter:
+        raise ParameterErrorException()
+    result.append(letter)
 
     if not scanner.current.type == CHAR:
-        return
+        return "".join(result)
     if scanner.current.spelling.isalpha() or scanner.current.spelling.isdigit():
-        parse_letter_digit_string(scanner)
+        result.append(parse_letter_digit_string(scanner))
+    return "".join(result)
 
 
 def parse_letter_digit_string(scanner):
-    parse_letter_digit(scanner)
+    result = [parse_letter_digit(scanner)]
 
     if not scanner.current.type == CHAR:
-        return
+        return "".join(result)
     while scanner.current.spelling.isalpha() or scanner.current.spelling.isdigit():
-        parse_letter_digit(scanner)
+        result.append(parse_letter_digit(scanner))
+    return "".join(result)
 
 
 def parse_letter_digit(scanner):
     if scanner.current.type != CHAR:
-        raise ParseException("let-dig")
+        raise ParameterErrorException()
     if scanner.current.spelling.isalpha() or scanner.current.spelling.isdigit():
+        result = scanner.current.spelling
         scanner.accept(CHAR, scanner.current.spelling)
+        return result
     else:
-        raise ParseException("let_dig")
+        raise ParameterErrorException()
+
+
+def parse_rcpt_to(scanner):
+    try:
+        if not accept_literal_str(scanner, "RCPT"):
+            raise UnrecognizedCommandException()
+        parse_whitespace(scanner)
+        if not accept_literal_str(scanner, "TO:"):
+            raise UnrecognizedCommandException()
+    except ParseException:
+        raise UnrecognizedCommandException()
+
+    parse_nullspace(scanner)
+    path = parse_path(scanner)
+    parse_nullspace(scanner)
+    if not scanner.accept(NEWLINE):
+        raise ParameterErrorException()
+    return path
+
+
+def parse_data_cmd(scanner):
+    if not accept_literal_str(scanner, "DATA"):
+        raise UnrecognizedCommandException()
+    parse_nullspace(scanner)
+    if not scanner.accept(NEWLINE):
+        raise ParameterErrorException()
+
+
+def parse_data_txt():
+    result = []
+    for line in sys.stdin:
+        print(line, end="")
+        if line.rstrip() == ".":
+            return "".join(result)
+        result.append(line)
+
+
+def try_parse(line, expected, funcs):
+    if line.strip() == "":
+        raise UnrecognizedCommandException()
+    stream = io.StringIO(line)
+    tok_scanner = TokenScanner(stream)
+    f = funcs[tok_scanner.current.spelling[0]]
+    if f:
+        try:
+            parsed = f(tok_scanner)
+            if f not in expected:
+                raise OutOfOrderException()
+            return f, parsed
+        except ParameterErrorException:
+            if f not in expected:
+                raise OutOfOrderException()
+            else:
+                raise
+    else:
+        raise UnrecognizedCommandException()
+
+
+PARSERS = {"M": parse_mail_from_cmd, "R": parse_rcpt_to, "D": parse_data_cmd}
+
+
+def read_next_line():
+    try:
+        line = next(sys.stdin)
+        print(line, end="")
+        return line
+    except StopIteration:
+        exit(0)
+
+
+def accept_rcpt_to():
+    rcpts = []
+    rcpt_line = read_next_line()
+    f, rcpt_addr = try_parse(rcpt_line, [parse_rcpt_to], PARSERS)
+    while f == parse_rcpt_to:
+        print(OK)
+        rcpts.append(rcpt_addr)
+        rcpt_line = read_next_line()
+        f, rcpt_addr = try_parse(rcpt_line, [parse_rcpt_to, parse_data_cmd], PARSERS)
+    return rcpts
+
+
+def accept_data_entry():
+    print(ENTER_DATA)
+    return parse_data_txt()
+
+
+def accept_mail_from():
+    mail_from = read_next_line()
+    f, from_addr = try_parse(mail_from, [parse_mail_from_cmd], PARSERS)
+    print(OK)
+    rcpts = accept_rcpt_to()
+    text = accept_data_entry()
+    print(OK)
+    return Mail(from_addr, rcpts, text)
+
+
+def mail_to_string(mail):
+    result = []
+    result.append("From: ")
+    result.append("<")
+    result.append(mail.src)
+    result.append(">\n")
+    for rcpt in mail.targets:
+        result.append("To: ")
+        result.append("<")
+        result.append(rcpt)
+        result.append(">\n")
+    result.append(mail.text)
+    return "".join(result)
 
 
 if __name__ == "__main__":
-    for line in sys.stdin:
-        print(line, end="")
-        stream = io.StringIO(line)
-        tok_scanner = TokenScanner(stream)
+    while True:
         try:
-            parse_mail_from_cmd(tok_scanner)
-            print("Sender ok")
+            mail = accept_mail_from()
+            mail_to_save = mail_to_string(mail)
+            for file_name in mail.targets:
+                with open("forward/" + file_name, "a") as file:
+                    file.write(mail_to_save)
         except ParseException as e:
             print(e.args[0])
