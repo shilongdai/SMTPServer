1e5d04fe943660bad65fc371ebd13eddefe2339d
Shilong Dai <sdai@viperfish.net>
2021-10-18 11:15:31 -0400

    finished client server

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 import io
 import sys
 from collections import namedtuple
+import socket
+import multiprocessing as mtp
 
 # Token types
 CHAR = 1
@@ -15,12 +17,20 @@ COMMAND_UNREC = "500 Syntax error: command unrecognized"
 PARAM_ERROR = "501 Syntax error in parameters or arguments"
 OUT_OF_ORDER_ERROR = "503 Bad sequence of commands"
 
+# Connection
+HOST_NAME = "localhost"
+PORT = 9900
+MAX_CONN_QUEUE = 5
+
 # OK Types
 OK = 250
 ENTER_DATA = 354
+GREETING = 220
+QUIT = 221
 
 OK_MSG = "%d OK" % OK
 ENTER_DATA_MSG = "%d Start mail input; end with <CRLF>.<CRLF>" % ENTER_DATA
+QUIT_MSG = "%d %s closing connection" % (QUIT, HOST_NAME)
 
 Token = namedtuple("Token", ["type", "spelling"])
 
@@ -85,12 +95,28 @@ class OutOfOrderException(ParseException):
         ParseException.__init__(self, OUT_OF_ORDER_ERROR)
 
 
+class MailBox:
+
+    def __init__(self, user, domain):
+        self.user = user
+        self.domain = domain
+
+    def __str__(self):
+        return self.user + "@" + self.domain
+
+
 class Mail:
 
     def __init__(self, src, rcpts, text):
         self.src = src
         self.targets = rcpts
         self.text = text
+
+
+class ClientQuit(BaseException):
+
+    def __init__(self):
+        BaseException.__init__(self)
 
 
 def accept_literal_str(scanner, string):
@@ -144,7 +170,7 @@ def parse_mailbox(scanner):
     if not scanner.accept(SPEC, "@"):
         raise ParameterErrorException()
     domain = parse_domain(scanner)
-    return user + "@" + domain
+    return MailBox(user, domain)
 
 
 def parse_string(scanner):
@@ -235,10 +261,20 @@ def parse_data_cmd(scanner):
         raise ParameterErrorException()
 
 
-def parse_data_txt():
+def parse_helo_cmd(scanner):
+    if not accept_literal_str(scanner, "HELO"):
+        raise UnrecognizedCommandException()
+    parse_whitespace(scanner)
+    domain = parse_domain(scanner)
+    parse_nullspace(scanner)
+    if not scanner.accept(NEWLINE):
+        raise ParameterErrorException()
+    return domain
+
+
+def parse_data_txt(server_in, server_out):
     result = []
-    for line in sys.stdin:
-        print(line, end="")
+    for line in server_in:
         if line.rstrip() == ".":
             return "".join(result)
         result.append(line)
@@ -267,42 +303,48 @@ def try_parse(line, expected, funcs):
         raise UnrecognizedCommandException()
 
 
-PARSERS = {"M": parse_mail_from_cmd, "R": parse_rcpt_to, "D": parse_data_cmd}
+PARSERS = {"M": parse_mail_from_cmd, "R": parse_rcpt_to, "D": parse_data_cmd, "H": parse_helo_cmd}
 
 
-def read_next_line():
+def handle_quit(server_out):
+    print(QUIT_MSG, file=server_out, flush=True)
+
+
+def read_next_line(server_in, server_out):
     try:
-        line = next(sys.stdin)
-        print(line, end="")
+        line = next(server_in)
+        if line.strip() == "QUIT":
+            handle_quit(server_out)
+            raise ClientQuit()
         return line
     except StopIteration:
         exit(0)
 
 
-def accept_rcpt_to():
+def accept_rcpt_to(server_in, server_out):
     rcpts = []
-    rcpt_line = read_next_line()
+    rcpt_line = read_next_line(server_in, server_out)
     f, rcpt_addr = try_parse(rcpt_line, [parse_rcpt_to], PARSERS)
     while f == parse_rcpt_to:
-        print(OK_MSG)
+        print(OK_MSG, file=server_out, flush=True)
         rcpts.append(rcpt_addr)
-        rcpt_line = read_next_line()
+        rcpt_line = read_next_line(server_in, server_out)
         f, rcpt_addr = try_parse(rcpt_line, [parse_rcpt_to, parse_data_cmd], PARSERS)
     return rcpts
 
 
-def accept_data_entry():
-    print(ENTER_DATA_MSG)
-    return parse_data_txt()
+def accept_data_entry(server_in, server_out):
+    print(ENTER_DATA_MSG, file=server_out, flush=True)
+    return parse_data_txt(server_in, server_out)
 
 
-def accept_mail_from():
-    mail_from = read_next_line()
+def accept_mail_from(server_in, server_out):
+    mail_from = read_next_line(server_in, server_out)
     f, from_addr = try_parse(mail_from, [parse_mail_from_cmd], PARSERS)
-    print(OK_MSG)
-    rcpts = accept_rcpt_to()
-    text = accept_data_entry()
-    print(OK_MSG)
+    print(OK_MSG, file=server_out, flush=True)
+    rcpts = accept_rcpt_to(server_in, server_out)
+    text = accept_data_entry(server_in, server_out)
+    print(OK_MSG, file=server_out, flush=True)
     return Mail(from_addr, rcpts, text)
 
 
@@ -310,24 +352,71 @@ def mail_to_string(mail):
     result = []
     result.append("From: ")
     result.append("<")
-    result.append(mail.src)
+    result.append(str(mail.src))
     result.append(">\n")
     for rcpt in mail.targets:
         result.append("To: ")
         result.append("<")
-        result.append(rcpt)
+        result.append(str(rcpt))
         result.append(">\n")
     result.append(mail.text)
     return "".join(result)
 
 
+def accept_helo(server_in, server_out):
+    helo = read_next_line(server_in, server_out)
+    f, helo_msg = try_parse(helo, (parse_helo_cmd,), PARSERS)
+    print("%d %s pleased to meet you" % (OK, helo_msg), file=server_out, flush=True)
+
+
+def process_request(client_conn):
+    try:
+        with client_conn.makefile(mode="r", newline="\n") as server_in, client_conn.makefile(mode="w") as server_out:
+            helo_accepted = False
+            print("%d %s" % (GREETING, HOST_NAME), file=server_out, flush=True)
+            while not helo_accepted:
+                try:
+                    accept_helo(server_in, server_out)
+                    helo_accepted = True
+                except ParseException as e:
+                    print(e.args[0], file=server_out, flush=True)
+            while True:
+                try:
+                    mail = accept_mail_from(server_in, server_out)
+                    mail_to_save = mail_to_string(mail)
+                    file_names = set([box.domain for box in mail.targets])
+                    for file_name in file_names:
+                        with open("forward/" + file_name, "a") as file:
+                            file.write(mail_to_save)
+                except ParseException as e:
+                    print(e.args[0], file=server_out, flush=True)
+    except ClientQuit:
+        return
+    except Exception as final_error:
+        print(final_error)
+    finally:
+        client.shutdown(socket.SHUT_RDWR)
+        client_conn.close()
+
+
 if __name__ == "__main__":
-    while True:
-        try:
-            mail = accept_mail_from()
-            mail_to_save = mail_to_string(mail)
-            for file_name in mail.targets:
-                with open("forward/" + file_name, "a") as file:
-                    file.write(mail_to_save)
-        except ParseException as e:
-            print(e.args[0])
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except Exception as e:
+        print(e)
+        exit(0)
+    try:
+        server_socket.bind(("", PORT))
+        server_socket.listen(MAX_CONN_QUEUE)
+
+        while True:
+            client, addr = server_socket.accept()
+            process = mtp.Process(target=process_request, args=(client,))
+            process.start()
+
+    except Exception as e:
+        print(e)
+    finally:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.close()
+
--- sender.py
+++ sender.py
@@ -1,96 +1,97 @@
 import io
 import sys
+import socket
 
+import parse
 from parse import ENTER_DATA
 from parse import Mail
 from parse import OK
-from parse import OutOfOrderException
+from parse import GREETING
 from parse import ParseException
-from parse import UnrecognizedCommandException
-from parse import accept_literal_str
-from parse import parse_path
-from parse import try_parse
+from parse import TokenScanner
+from parse import parse_mailbox
 
 
-def parse_from_header(scanner):
-    if not accept_literal_str(scanner, "From: "):
-        raise UnrecognizedCommandException()
-    return parse_path(scanner)
+INCORRECT_FROM_PATH = "Incorrect from path, please try again"
+INCORRECT_TO_PATH = "Incorrect to path, please try again"
+UNCLEAN_EXIT = "Failed to gracefully quit from server"
+UNEXPECTED_CONNECTION_CLOSE = "Server closed connection unexpectedly"
+FAILED_TO_ENTER_MAIL_FROM = "Server did not accept source address"
+FAILED_TO_ENTER_RCPTS = "Server did not accept recipients"
+SERVER_NOT_READY_FOR_DATA = "Server did not want to accept message data"
+SERVER_REJECT_DATA = "Server rejected message data"
+INVALID_SERVER_GREETING = "Server failed to greet properly"
 
 
-def parse_to_header(scanner):
-    if not accept_literal_str(scanner, "To: "):
-        raise UnrecognizedCommandException()
-    return parse_path(scanner)
+CONNECTION_TIMEOUT = 5
 
 
-PARSERS = {"F": parse_from_header, "T": parse_to_header}
-
-
-def read_forward_file(file):
-    try:
-        next_line = file.readline()
-        return next_line
-    except IOError:
-        exit(0)
+def try_parse_mailbox(line):
+    stream = io.StringIO(line)
+    tok_scanner = TokenScanner(stream)
+    return parse_mailbox(tok_scanner)
 
 
 def read_from_line(file):
-    from_line = read_forward_file(file)
-    if len(from_line) == 0:
-        return None
-    f, parsed = try_parse(from_line, [parse_from_header], PARSERS)
+    parsed = None
+    while parsed is None:
+        print("From:")
+        from_line = next(file)
+        try:
+            parsed = try_parse_mailbox(from_line)
+        except ParseException:
+            print(INCORRECT_FROM_PATH)
     return parsed
 
 
 def read_to_lines(file):
     result = []
-    to_line = read_forward_file(file)
-    f, parsed = try_parse(to_line, [parse_to_header], PARSERS)
-    result.append(parsed)
-    while True:
-        position = file.seek(0, io.SEEK_CUR)
-        to_line = read_forward_file(file)
+    while len(result) == 0:
+        print("To:")
+        to_combo = next(file)
+        separate_tos = [s.strip() for s in to_combo.split(",")]
         try:
-            f, parsed = try_parse(to_line, [parse_to_header], PARSERS)
-            result.append(parsed)
-        except (UnrecognizedCommandException, OutOfOrderException):
-            file.seek(position)
-            break
+            for to_line in separate_tos:
+                parsed = try_parse_mailbox(to_line)
+                result.append(parsed)
+        except ParseException:
+            print(INCORRECT_TO_PATH)
+            result = []
     return result
 
 
 def read_data(file):
-    result = []
+    print("Subject:")
+    result = ["Subject: %s" % next(file), "\n"]
+    print("Message:")
     while True:
-        position = file.seek(0, io.SEEK_CUR)
-        data_line = read_forward_file(file)
-        if len(data_line) == 0:
+        data_line = next(file)
+        if data_line == ".\n":
             break
-        try:
-            try_parse(data_line, [parse_from_header], PARSERS)
-            file.seek(position)
-            break
-        except ParseException:
-            result.append(data_line)
+        result.append(data_line)
     return "".join(result)
 
 
 def read_mail(file):
     from_header = read_from_line(file)
-    if not from_header:
-        return None
     to_headers = read_to_lines(file)
     data = read_data(file)
+    header = ["From: ", "<%s>\n" % from_header, "To: "]
+    for to in to_headers:
+        header.append("<%s>" % str(to))
+        header.append(", ")
+    del header[-1]
+    header.append("\n")
+    data = "".join(header) + data
     return Mail(from_header, to_headers, data)
 
 
 def read_server_output(stream):
     try:
         output = next(stream)
-        print(output, file=sys.stderr, end="")
         return output
     except (IOError, StopIteration):
+        print(UNEXPECTED_CONNECTION_CLOSE)
         exit(0)
 
 
@@ -111,61 +112,109 @@ def match_code(resp, code):
     return code == code_int
 
 
-def exit_sequence(server_out):
-    print("QUIT", file=server_out)
-    exit(0)
+def exit_sequence(server_in, server_out):
+    print("QUIT", file=server_out, flush=True)
+    goodbye = read_server_output(server_in)
+    if match_code(goodbye, parse.QUIT):
+        exit(0)
+    else:
+        print(UNCLEAN_EXIT)
+        exit(0)
 
 
 def handle_mail_from(from_addr, server_in, server_out):
-    print("MAIL FROM: <%s>" % from_addr, file=server_out)
+    print("MAIL FROM: <%s>" % from_addr, file=server_out, flush=True)
     resp = read_server_output(server_in)
     if not match_code(resp, OK):
-        exit_sequence(server_out)
+        print(FAILED_TO_ENTER_MAIL_FROM)
+        exit_sequence(server_in, server_out)
 
 
 def handle_mail_to(to_addrs, server_in, server_out):
     for addr in to_addrs:
-        print("RCPT TO: <%s>" % addr, file=server_out)
+        print("RCPT TO: <%s>" % str(addr), file=server_out, flush=True)
         resp = read_server_output(server_in)
         if not match_code(resp, OK):
-            exit_sequence(server_out)
+            print(FAILED_TO_ENTER_RCPTS)
+            exit_sequence(server_in, server_out)
 
 
 def handle_data(data, server_in, server_out):
-    print("DATA", file=server_out)
+    print("DATA", file=server_out, flush=True)
     resp = read_server_output(server_in)
     if not match_code(resp, ENTER_DATA):
-        exit_sequence(server_out)
-    print(data, file=server_out, end="")
+        print(SERVER_NOT_READY_FOR_DATA)
+        exit_sequence(server_in, server_out)
+    print(data, file=server_out, end="", flush=True)
     if len(data) == 0:
-        print(".")
+        print(".", file=server_out, flush=True)
     elif data[-1] == "\n":
-        print(".")
+        print(".", file=server_out, flush=True)
     else:
-        print("\n.")
+        print("\n.", file=server_out, flush=True)
     resp = read_server_output(server_in)
     if not match_code(resp, OK):
-        exit_sequence(server_out)
+        print(SERVER_REJECT_DATA)
+        exit_sequence(server_in, server_out)
 
 
-def process_mails(path, server_in, server_out):
+def greeting_sequence(server_in, server_out):
+    domain = get_my_domain()
+    server_greeting = read_server_output(server_in)
+    if not match_code(server_greeting, GREETING):
+        print(INVALID_SERVER_GREETING)
+        exit_sequence(server_in, server_out)
+    print("HELO %s" % domain, file=server_out, flush=True)
+    server_resp = read_server_output(server_in)
+    if not match_code(server_resp, OK):
+        print(INVALID_SERVER_GREETING)
+        exit_sequence(server_in, server_out)
+
+
+def get_my_domain():
+    fqdn = socket.getfqdn()
+    separated = fqdn.split(".")
+    if len(separated) == 1:
+        return separated[0]
+    else:
+        return ".".join(separated[1:])
+
+
+def process_mails(host, port):
     try:
-        with open(path, "r") as file:
-            while True:
-                try:
-                    next_mail = read_mail(file)
-                    if not next_mail:
-                        exit_sequence(server_out)
-                except (ParseException, IOError):
-                    exit_sequence(server_out)
-                handle_mail_from(next_mail.src, server_in, server_out)
-                handle_mail_to(next_mail.targets, server_in, server_out)
-                handle_data(next_mail.text, server_in, server_out)
-    except IOError:
+        next_mail = read_mail(sys.stdin)
+        if not next_mail:
+            return
+    except StopIteration:
         return
+    except IOError as e:
+        print(e)
+        return
+    try:
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.settimeout(CONNECTION_TIMEOUT)
+        client_sock.connect((host, port))
+    except OSError as e:
+        print(e)
+        return
+    try:
+        with client_sock.makefile(mode="r", newline="\n") as server_in, client_sock.makefile(mode="w") as server_out:
+            greeting_sequence(server_in, server_out)
+            handle_mail_from(next_mail.src, server_in, server_out)
+            handle_mail_to(next_mail.targets, server_in, server_out)
+            handle_data(next_mail.text, server_in, server_out)
+            exit_sequence(server_in, server_out)
+    finally:
+        client_sock.shutdown(socket.SHUT_RDWR)
+        client_sock.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
+        print("Please input mail server and port")
         exit(0)
-    process_mails(sys.argv[1], sys.stdin, sys.stdout)
+    try:
+        port = int(sys.argv[2])
+        process_mails(sys.argv[1], port)
+    except Exception as e:
+        print(e)
